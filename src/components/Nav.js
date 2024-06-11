02c12d17609ceab388e056e19bbfb75bc7a92f94
Arivola Ursulla <ursullaarivola@gmail.com>
2024-06-10 20:39:45 -0400

    2nd commit

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,30 @@
 import React from 'react';
 //import icons
-import {BiHomeAlt, BiUser} from 'react-icons/bi';
-import {BsClipboardData, BsBriefcase, BsChatSquare} from 'react-icons/bs';
+import {BiClipboard, BiHomeAlt, BiUser} from 'react-icons/bi';
+import {BsClipboardData, BsBriefcase, BsChatSquare, BsChat, BsChatSquareText} from 'react-icons/bs';
 //link
 import {Link} from 'react-scroll';
 
 const Nav = () => {
   return (
-  <nav>
+  <nav className='fixed bottom-2 lg:bottom-8 w-full overflow-hidden z-50'>
     <div className='container mx-auto'>
       {/* nav inner */}
-      <div>
-        <Link>
+      <div className='w-full bg-black/20 h-[96px] backdrop-blur-2xl rounded-full max-w-[460px] mx-auto px-5 flex justify-between text-2xl text-white/50'>
+        <Link to='home' className='cursor-pointer w-[90px] h-[90px] flex items-center justify-center'>
           <BiHomeAlt />
+        </Link>
+        <Link to='about' className='cursor-pointer w-[90px] h-[90px] flex items-center justify-center'>
+          <BiUser />
+        </Link>
+        <Link to='services' className='cursor-pointer w-[90px] h-[90px] flex items-center justify-center'>
+          <BsClipboardData />
+        </Link>
+        <Link to='work' className='cursor-pointer w-[90px] h-[90px] flex items-center justify-center'>
+          <BsBriefcase />
+        </Link>
+        <Link to='contact' className='cursor-pointer w-[90px] h-[90px] flex items-center justify-center'>
+          <BsChatSquareText />
         </Link>
       </div>
     </div>
